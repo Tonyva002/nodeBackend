b477a9c9137880fd4cd7193f9d55328d07ada941
Tony Vasquez Arias <tonyva002@hotmail.com>
2025-05-13 00:08:28 -0400

    Update config file

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'delivery'
+    database: 'concentra'
 });
 
 db.connect(function(err){
